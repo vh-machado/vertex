2fc7393cfaa6a6580f28e8015d20a88a189866fe
Marcus Loureiro <marcushuriel08@hotmail.com>
2022-06-29 11:55:59 -0300

    Algoritmo de planaridade implementado

---
+++ src/Algoritmos/criaObjetoPlanar.js
@@ -0,0 +1,4 @@
+export function criaObjetoPlanar(nodes, edges){
+
+    
+}
--- src/components/GraphResults.js
+++ src/components/GraphResults.js
@@ -4,11 +4,16 @@ import {viewCard, viewCardSelection} from '../components/CardInfo';
 import {algoritmosGrafos} from '../Algoritmos/funcoesBasicas'
 import { Select } from '@chakra-ui/react'
 import { dijkstra } from '../Algoritmos/dijkstra';
-import { criaMatrizAdjacencia } from '../Algoritmos/criaMatrizAdjacencia'
+import { criaMatrizAdjacencia } from '../Algoritmos/criaMatrizAdjacencia';
+import {planarity_test} from '../Algoritmos/planarFunction';
+
 
 
 const teste = new algoritmosGrafos()
 var algDijkstra = new dijkstra()
+
+
+
 
 
 
@@ -35,6 +40,28 @@ var state = {
   },
 };
 
+var K5 = {
+    nodes: [
+      { id: 0, label: 'A', x: 200, y: 0 },
+      { id: 1, label: 'B', x: 50, y: 250 },
+      { id: 2, label: 'C', x: 300, y: 0 },
+      { id: 3, label: 'D', x: 90, y: 100 },
+      { id: 4, label: 'E', x: 0, y: 10 },
+    ],
+    edges: [
+      { from: 0, to: 1, label: 3 },
+      { from: 0, to: 2, label: 5 },
+      { from: 0, to: 3, label: 1 },
+      { from: 0, to: 4, label: 3 },
+      { from: 1, to: 2, label: 9 },
+      { from: 1, to: 3, label: 4 },
+      { from: 1, to: 4, label: 6},
+      { from: 2, to: 3, label: 9 },
+      { from: 2, to: 4, label: 4 },
+      { from: 3, to: 4, label: 6},
+    ],
+};
+
 const origem = state.graph.edges[0].from
 const destino = state.graph.edges[1].to
 const vertice = state.graph.nodes[0]
@@ -47,16 +74,16 @@ const resultadoConexo = teste.eConexo(grafo);
 const resultadoCiclico = teste.possuiCiclo(grafo, vertice.id, grafo.nodes[5].id)
 
 
-const resulatdoDijkstra = algDijkstra.dijkstra(criaMatrizAdjacencia(grafo.nodes, grafo.edges))
+/*const resulatdoDijkstra = algDijkstra.dijkstra(criaMatrizAdjacencia(grafo.nodes, grafo.edges))
 const resultadoMenorCaminho = resulatdoDijkstra.path.toString();
-const resultadoMenorCusto = resulatdoDijkstra.distance;
+const resultadoMenorCusto = resulatdoDijkstra.distance;*/
 
 
 const resultadoFrConexo = "Sim";
 const resultadoUnConexo = "Não";
 const resultadoFoConexo = "Não";
 
-const resultadoPlanar = "Sim";
+const resultadoPlanar = planarity_test(state.graph.nodes, state.graph.edges);
 const resultadoEuleriano = "Não";
 
 
@@ -77,10 +104,10 @@ function GraphResults() {
     {viewCard('Componentes Fortes:', 'A-B-C')}
     {viewCard('Dígrafo Unilateralmente Conexo?', resultadoUnConexo)}
     {viewCard('Dígrafo Fortemente Conexo?', resultadoFoConexo)}
-    {viewCard('Grafo Planar?', resultadoPlanar)}
+    {viewCard('Grafo Planar?', resultadoPlanar.toString())}
     {viewCard('Grafo Euleriano?', resultadoEuleriano)}
-    {viewCard('Menor Caminho:', resultadoMenorCaminho)}
-    {viewCard('Menor Custo:', resultadoMenorCusto)}
+    {/*viewCard('Menor Caminho:', resultadoMenorCaminho)*/}
+    {/*viewCard('Menor Custo:', resultadoMenorCusto)*/}
     {viewCard('Grafo Ciclico:', resultadoCiclico.toString())}
     
     </>
