49d69eaee6adc5a3426382dacea052e7aebab831
Filipe Sales <eduardojs999@gmail.com>
2022-12-06 15:28:37 -0500

    fix: adjust bfs algorithm

--- src/Algoritmos/profundidade.js
+++ src/Algoritmos/profundidade.js
@@ -1,51 +1,58 @@
-/**
- * Implementation of Breadth-First-Search (BFS) using adjacency matrix.
- * This returns nothing (yet), it is meant to be a template for whatever you want to do with it,
- * e.g. finding the shortest path in a unweighted graph.
- * This has a runtime of O(|V|^2) (|V| = number of Nodes), for a faster implementation see @see ../fast/BFS.java (using adjacency Lists)
- *
- * @param graph an adjacency-matrix-representation of the graph where (x,y) is true if the the there is an edge between nodes x and y.
- * @param start the node to start from.
- * @return Array array containing the shortest distances from the given start node to each other node
- */
- const bfs = function (graph, start) {
-    //A Queue to manage the nodes that have yet to be visited
-    var queue = [];
-    //Adding the node to start from
-    queue.push(start);
-    //A boolean array indicating whether we have already visited a node
-    var visited = [];
-    //(The start node is already visited)
-    visited[start] = true;
-    // Keeping the distances (might not be necessary depending on your use case)
-    var distances = []; // No need to set initial values since every node is visted exactly once
-    //(the distance to the start node is 0)
-    distances[start] = 0;
-    //While there are nodes left to visit...
-    while (queue.length > 0) {
-      console.log('Visited nodes: ' + visited);
-      console.log('Distances: ' + distances);
-      var node = queue.shift();
-      console.log('Removing node ' + node + ' from the queue...');
-      //...for all neighboring nodes that haven't been visited yet....
-      for (var i = 1; i < graph[node].length; i++) {
-        if (graph[node][i] && !visited[i]) {
-          // Do whatever you want to do with the node here.
-          // Visit it, set the distance and add it to the queue
-          visited[i] = true;
-          distances[i] = distances[node] + 1;
-          queue.push(i);
-          console.log(
-            'Visiting node ' +
-              i +
-              ', setting its distance to ' +
-              distances[i] +
-              ' and adding it to the queue'
-          );
+class Graph {
+  constructor(v) {
+    this.time = 0;
+    this.traversal_array = [];
+    this.v = v;
+    this.e = randomIntFromInterval(9, 45);
+    this.graph_list = [];
+    this.graph_matrix = [];
+  }
+
+  randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getNumberOfEdges() {
+    return this.e;
+  }
+
+  dfs() {
+    this.visited = [false] * this.v;
+    this.start_time = [0] * this.v;
+    this.end_time = [0] * this.v;
+  }
+
+  traverse_dfs(node) {
+    this.visited[node] = true;
+    this.traversal_array.push(node);
+    this.start_time[node] = this.time;
+    this.time += 1;
+
+    for (let neighbour in this.graph_list[node]) {
+      if (!self.visited[neighbour]) {
+        console.log('Tree edge: ', node, '-->', neighbour);
+        self.traverse_dfs(neighbour);
+      } else {
+        if (
+          this.start_time[node] > this.start_time[neighbour] &&
+          this.end_time[node] < this.end_time[neighbour]
+        ) {
+          console.log('Back edge: ', node, '-->', neighbour);
+        } else if (
+          this.start_time[node] < this.start_time[neighbour] &&
+          this.end_time[node] > this.end_time[neighbour]
+        ) {
+          console.log('Forward Edge: ', node, '-->', neighbour);
+        } else if (
+          this.start_time[node] > this.start_time[neighbour] &&
+          this.end_time[node] > this.end_time[neighbour]
+        ) {
+          console.log('Cross Edge:', node, '-->', neighbour);
         }
       }
+      this.end_time[node] = this.time;
+      this.time += 1;
     }
-    console.log('No more nodes in the queue. Distances: ' + distances);
-    return distances;
-  };
-  
+  }
+}
