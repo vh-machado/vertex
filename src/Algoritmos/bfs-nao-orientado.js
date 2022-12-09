587a4c98b203126f4569fd29676e540dad86c0c5
Filipe Sales <eduardojs999@gmail.com>
2022-12-08 21:28:46 -0500

    feat: DFS non oriented implemented

--- src/Algoritmos/bfs-nao-orientado.js
+++ src/Algoritmos/bfs-nao-orientado.js
@@ -19,4 +19,35 @@ function DFS(graph, start, visited = new Set()) {
   };
   
   console.log(DFS(graph, 'a'));
+
+
+  //BFS non-oriented graph
+function BFS(graph, start, visited = new Set()) {
+    const queue = [start];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      visited.add(node);
+      for (let child of graph[node]) {
+        if (!visited.has(child)) {
+          queue.push(child);
+        }
+      }
+    }
+    return visited;
+  }
+  
+  const graph2 = {
+    a: ['b', 'c'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: ['f'],
+    f: []
+  };
+  
+  console.log(BFS(graph, 'a'));
+
+
+//BFS non-oriented graph
+
   
